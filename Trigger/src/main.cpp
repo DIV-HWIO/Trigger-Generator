a5d7939ffa92def4b20a5ae740b436b8be6d26bb
Jaeyoung Jeon <aainka@gmail.com>
2023-11-13 11:30:35 +0900

    Update main.cpp

--- Trigger/src/main.cpp
+++ Trigger/src/main.cpp
@@ -214,7 +214,7 @@ void loop()
 
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<head><meta charset='UTF-8' name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\"></head>");
 
             client.println("<body><a href=\"http://" + WiFi.localIP().toString() + "\" style=\"text-decoration: none; color: inherit;\"><h1 class='red'><img src='http://www.dongiltech.co.kr/images/common/logo.png' alt='DIT'>   TRIGGER GENERATOR V0.01</h1></a>");
@@ -223,16 +223,28 @@ void loop()
 
             // h1 redirect
             // client.println("<body><a href=" + WiFi.localIP().toString() + " <style=\"text-decoration: none; color: inherit;\"><h1>DIV FPS Controller</h1></a>");
+            const String ss =  "<style>"\
+                "table { border-collapse: collapse;   padding: 10px; margin: 15px;}"\
+                ".red { color: #E03C32; font-size:40px; font-weight: bold; padding: 10px; margin: 10px;}"\
+                ".normal {  background-color: aliceblue; width: 800px; line-height: 1.5em; font-family: 'Nanum Gothic', sans-serif; font-size: 14px; padding: 10px; margin: 10px 40px;}"\
+                ".circle::before { content: '\\25CF'; margin-right: 0.5em; margin-left:20px}";
 
-            client.println("<style>");
-            client.println("table { border-collapse: collapse;   padding: 10px; margin: 15px;}");
-            client.println(".red { color: #E03C32; font-size:40px; font-weight: bold; padding: 10px; margin: 10px;}");
-            client.println(".xark::before { content: '\\2713'; margin-right: 0.5em; margin-left:20px}");
+        //    client.println("table { border-collapse: collapse;   padding: 10px; margin: 15px;}");
+            client.println(ss);
+         //   client.println(".normal {  background-color: aliceblue; width: 800px; font-family: 'Nanum Gothic', sans-serif; font-size: 16px; padding: 10px; margin: 10px;}");
+            client.println(".circle::before { content: '\\25CF'; margin-right: 0.5em; margin-left:20px}"); // check 2713 circle 25cf
             client.println("th, td { border: 1px solid #E03C32; text-align: right; padding: 7px; width:150px;}");
             client.println("</style>");
 
-             client.println("<h4 class='xark'>4 Channel Trigger Generator</h4> <h4 class='xark' >FPS 0.1 ~ 200 FPS (signal)</h4>");
-            client.println("<h4 class='xark' >GCount=" + String(GCount) + "</h4>");
+             client.println("<h4 class='circle'>4 Channel Trigger Generator</h4>");
+            client.println("<h4 class='circle' > 현재 진행 스템번호 =" + String(GCount) + "</h4>");
+            
+            const String a = "<div class='normal'>"\
+              "FPS 0.1 ~ 200 FPS (signal)<br>"
+              "초기설정 : AP모드/시리얼을 이용하여, 초기 AP(ssid,passwd)입력가능<br>"\
+              "표준IOT프로토콜 : MQTT 지원, Reset HW Chip <br>"\
+              "웹메뉴얼 연결 및 한글설명서 <br> </div>";
+           client.println(a);
 
             client.println("<table>");
             // Camera FPS Duration DownCount ExternTrigger Power
